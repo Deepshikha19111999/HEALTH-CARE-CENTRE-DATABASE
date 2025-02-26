008b4b40b3dd3ca92c9bfebe6ca18c43813d8cf6
Deepshikha19111999 <deepshikhapalai99@gmail.com>
2025-02-26 19:58:26 +0530

    department updating check

--- Heath_Care_Centre_Database/obj/Debug/netcoreapp2.1/Heath_Care_Centre_Database.AssemblyInfo.cs
+++ Heath_Care_Centre_Database/obj/Debug/netcoreapp2.1/Heath_Care_Centre_Database.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Heath_Care_Centre_Database")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab8276bba9b36f0e0de01a6cbd93aed570eb1fd0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1869c89acef134df682239aa10368c2fe3be9707")]
 [assembly: System.Reflection.AssemblyProductAttribute("Heath_Care_Centre_Database")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Heath_Care_Centre_Database")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
