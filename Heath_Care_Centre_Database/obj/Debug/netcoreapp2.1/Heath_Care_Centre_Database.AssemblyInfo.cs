53940068679c96e8a36c28e9e39c64ad34bf7a2e
racheeta <racheeta.s.m@gmail.com>
2025-03-07 16:25:50 +0530

    appointment table  updated

--- Heath_Care_Centre_Database/obj/Debug/netcoreapp2.1/Heath_Care_Centre_Database.AssemblyInfo.cs
+++ Heath_Care_Centre_Database/obj/Debug/netcoreapp2.1/Heath_Care_Centre_Database.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Heath_Care_Centre_Database")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cac2945d7258d9527f250d8f86d3ee2e2ecea1c5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71744e111e59fcb6f063499c09f1ae930751ddc1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Heath_Care_Centre_Database")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Heath_Care_Centre_Database")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
