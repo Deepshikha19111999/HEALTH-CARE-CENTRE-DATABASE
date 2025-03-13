3b3d1516239731f64d2e5f3deb870e8573060103
racheeta <racheeta.s.m@gmail.com>
2025-03-13 16:50:58 +0530

    some other changes

--- Heath_Care_Centre_Database/obj/Debug/netcoreapp2.1/Heath_Care_Centre_Database.AssemblyInfo.cs
+++ Heath_Care_Centre_Database/obj/Debug/netcoreapp2.1/Heath_Care_Centre_Database.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Heath_Care_Centre_Database")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bad8106b5a3f3612eb531b97f8251ee8907b81f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+795d4fd2eecfa0ffd45661fda7fa7fc592e539ae")]
 [assembly: System.Reflection.AssemblyProductAttribute("Heath_Care_Centre_Database")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Heath_Care_Centre_Database")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
